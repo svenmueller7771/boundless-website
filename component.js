5a5cbd52f9a283fc70bcd19d3b391be6bf640b96
svenmueller7771 <49875864+svenmueller7771@users.noreply.github.com>
2023-03-28 21:54:02 +0200

    Add files via upload

--- component.js
+++ component.js
@@ -21,13 +21,15 @@ class Header extends HTMLElement {
         <li class="navbar-item">
           <a class="navbar-link" href="products.html">Products</a>
           <div class="dropdown-content" id="dropdown">
-            <a href="standing-mirror.html">Standing mirror</a>
             <a href="acoustic-wood-slat-panel.html">Acoustic wood slat panel</a>
+            <a href="standing-mirror.html">Standing mirror</a>
             <a href="frame-gallery.html">Frame gallery</a>
             <a href="floating-frames.html">Floating frames</a>
-            <a href="backgammon-travel.html">Backgammon travel set</a>
             <a href="lego-coffee-table.html">Lego coffee table</a>
+            <a href="custom-tables.html">Live edge tables</a>
+            <a href="backgammon-travel.html">Backgammon travel set</a>
             <a href="fine-art-prints.html">Fine art prints</a>
+  
           </div>
         </li>
         <li class="navbar-item">
@@ -42,8 +44,51 @@ class Header extends HTMLElement {
         <span class="bar"></span>
         <span class="bar"></span>
       </div>
+      <ul class="navbar-menu" id="navbar-menu-cart">
+        <li class="navbar-item">
+          <a class="material-icons" id="navbar-link" href="" alt="profile">account_circle</a>
+        </li>
+        <li class="navbar-item">
+          <a class="material-icons" id="navbar-link" href="cart.html" alt="cart">shopping_bag</a>
+        </li>
+      </ul>
+
     </div>
   </nav>
+    `
+  }
+}
+
+
+class Section extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="info-section">
+    <div class="info-cards">
+      <div class="info-card">
+        <div class="material-icons">home_repair_service</div>
+        <h3 class="info-heading">Onsite installations</h3>
+        <p class="info-p">We're happy to help you achieve your goals. We offer an on-site service fee for installations (Western Cape only).</p>
+      </div>
+      <div class="info-card">
+        <div class="material-icons">local_shipping</div>
+        <h3 class="info-heading">Global shipping</h3>
+        <p class="info-p">Free shipping for orders greater than R1000. International orders are subject to our <a class="navbar-link" href="terms.html">shipping terms</a>.</p>
+      </div>
+      <div class="info-card">
+        <div class="material-icons">local_florist</div>
+        <h3 class="info-heading">Locally made by artisans</h3>
+        <p class="info-p">A curated selection of meticulously crafted goods, made locally by artisans in Cape Town.</p>
+      </div>
+      <div class="info-card">
+        <div class="material-icons">forest</div>
+        <h3 class="info-heading">Planting trees</h3>
+        <p class="info-p">For every item purchased, R50 is donated to the <a class="navbar-link"
+          href="https://greenpop.org/">Greenpop
+          Foundation</a>.</p>
+      </div>
+    </div>
+  </div>
     `
   }
 }
@@ -86,4 +131,4 @@ class Footer extends HTMLElement {
 
 customElements.define('global-navbar', Header);
 customElements.define('global-footer', Footer);
-
+customElements.define('info-section', Section);
